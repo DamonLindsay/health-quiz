d6f5ecd2514d7b70511942f5006a5813ff98dfdd
Damon Lindsay <damon.lindsay@my.jcu.edu.au>
2026-06-09 11:43:33 +1000

    feat: set up router with welcome, quiz and results routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,8 +1,27 @@
 import { createRouter, createWebHistory } from 'vue-router'
+import WelcomeView from '@/views/WelcomeView.vue'
+import QuizView from '@/views/QuizView.vue'
+import ResultsView from '@/views/ResultsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '/',
+      name: 'welcome',
+      component: WelcomeView,
+    },
+    {
+      path: '/quiz',
+      name: 'quiz',
+      component: QuizView,
+    },
+    {
+      path: '/results',
+      name: 'results',
+      component: ResultsView,
+    },
+  ],
 })
 
 export default router
